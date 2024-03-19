36b5002927823252e6ad7013175dde9adc15a60a
Pavel <sozonovps@gmail.com>
2024-03-19 12:19:09 +0800

    разработана программа

--- Lab23/Program.cs
+++ Lab23/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab23
@@ -10,10 +11,29 @@ namespace Lab23
     {
         static void Main(string[] args)
         {
-
-
+            Console.WriteLine("Введите число, из которого необходимо вычислить факториал");
+            int x = Convert.ToInt32(Console.ReadLine());
+            FactorialAsync(x);
+            Console.WriteLine("А больше я ничего не делаю, сижу починяю примус");
+            Thread.Sleep(500);
+            Console.WriteLine("Починил!");
+            Console.ReadLine();
         }
 
+        static void Factorial (int x)
+        {
+            int result = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                result *= i;   
+                Thread.Sleep(100);
+            }
+            Console.WriteLine($" Результат вычислений равен {result}.");
+        }
 
+        static async void FactorialAsync (int x)
+        {
+            await Task.Run(() => Factorial(x));
+        }
     }
 }
